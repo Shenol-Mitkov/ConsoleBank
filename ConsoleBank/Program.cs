e212ba2d5d9aab16270aaef2e6190b33640fa533
Shenol-Mitkov <shenolmitkov9@gmail.com>
2026-09-24 11:00:31 +0200

    Foldern var i en annan repo så flytta den till den rätta

---
+++ ConsoleBank/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Välkommen till ConsoleBank TM");
+            Console.WriteLine("Välj ditt konto och skriv in ditt lösenord ");
+            Console.WriteLine(" ");
+            Accounts();
+
+
+
+            Console.ReadKey();
+        }
+
+        static void Accounts()
+        {
+            string[] User = new string[5];
+            User[0] = "1: Shenol Mitkov";
+            User[1] = "2: Bertil Korggren";
+            User[2] = "3: Felix Reztsel";
+            User[3] = "4: Alexandra Nilsson";
+            User[4] = "5: Anas Alhussein";
+
+            Console.WriteLine(string.Join(Environment.NewLine, User));
+
+        }
+    }
+}
